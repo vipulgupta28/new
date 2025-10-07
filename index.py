32f9fdcbcb434d6ac2cfaefdc9797e68ba8a2158
VIPUL GUPTA <vilulgupta2802@gmail.com>
2025-10-07 17:07:50 +0530

    new

--- index.py
+++ index.py
@@ -21,4 +21,6 @@ def get_reel_stats():
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
